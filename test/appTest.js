021cf785ae8fafca3c5e6bc160bf434b68c22b73
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2021-12-02 22:39:57 +0600

    Added Coverage Support

--- test/appTest.js
+++ test/appTest.js
@@ -1,22 +1,28 @@
-const assert = require("chai").assert;
+import chai from "chai"
 
-describe("Our Testing Container", function() {
+import addNumbers from '../app.js';
+import subNumbers from '../sub.js';
+
+const assert = chai.assert;
+
+describe("Testing Container", function() {
 
     let a = 2,
         b = 5;
 
     // Summation Equality.
-    it.skip("Check Summation", function() {
-        assert.equal(a + b, 7);
+    it("Summation", function() {
+
+        let result = addNumbers(a, b);
+
+        assert.equal(result, 7);
     })
 
 
     // Subraction Chec
 
-    it("check subtraction", function() {
+    it("Subtraction", function() {
         assert.equal(b - a, 3);
     })
-
-
 
 })
