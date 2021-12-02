4eb2ed987ccb2e3c988b03e08f01264d7fb7f8f8
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2021-12-02 21:06:00 +0600

    First Commit

---
+++ test/appTest.js
@@ -0,0 +1,24 @@
+const assert = require("chai").assert;
+const X = require('../app');
+
+
+describe("Our Testing Container", function() {
+
+    let a = 2,
+        b = 5;
+
+    // Summation Equality.
+    it.skip("Check Summation", function() {
+        assert.equal(a + b, 7);
+    })
+
+
+    // Subraction Chec
+
+    it("check subtraction", function() {
+        assert.equal(b - a, 3);
+    })
+
+
+
+})
